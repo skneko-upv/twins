e69f042125300f9c353dd33d982ae945132dc842
jesgarsas <jesgarsas@gmail.com>
2020-04-17 16:57:01 +0200

    Añadido efectos para el cardcomponent

--- Twins/Twins/Components/CardComponent.xaml.cs
+++ Twins/Twins/Components/CardComponent.xaml.cs
@@ -13,33 +13,56 @@ namespace Twins.Components
     public partial class CardComponent : StackLayout
     {
         private Card Card { get; }
+        private ImageSource BackImage { get; }
 
         public CardComponent()
         {
             InitializeComponent();
+            BackImage = ImageSource.FromFile("Assets/Cards/backimage.png");
         }
 
-        public CardComponent(Card card) 
+        public CardComponent(Card card)
         {
             InitializeComponent();
+            BackImage = ImageSource.FromFile("Assets/Cards/backimage.png");
             Card = card;
         }
 
         private async void CardClicked(object sender, EventArgs e)
         {
-            if (!Card.Flipped) 
+            if (!Card.Flipped)
             {
-                Card.Flipped = true;
-                await AnimationFlip(90);
-                button.ImageSource = Card.Image;
-                await AnimationFlip(180);
+                await CardFlip();
             }
         }
 
-        private async Task AnimationFlip(int angle) 
+        private async Task AnimationFlip(int angle, uint seconds) 
         {
-            await button.RotateYTo(angle, 250);
+            await button.RotateYTo(angle, seconds);
         }
 
+
+        public async Task CardFlip() 
+        {
+            Card.Flipped = true;
+            await AnimationFlip(90, 150);
+            button.ImageSource = Card.Image;
+            await AnimationFlip(180, 150);
+        }
+
+        public async Task CardUnflip()
+        {
+            Card.Flipped = false;
+            await AnimationFlip(90, 150);
+            button.ImageSource = BackImage;
+            await AnimationFlip(0, 150);
+        }
+
+        public async Task CardMatched() 
+        {
+            button.RotationY = 0;
+            await AnimationFlip(360, 250);
+            button.RotationY = 0;
+        }
     }
 }
