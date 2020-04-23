f169f15f5467161c3b6b1caab9acbcb17def332e
Neko <software@skneko.moe>
2020-04-23 21:20:47 +0200

    Añadir base de ViewModel

---
+++ Twins/Twins/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Twins.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Convention convenience private method")]
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
