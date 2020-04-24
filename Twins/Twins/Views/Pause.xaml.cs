bd3bed7f5ddcd355cc9f4d07df4df64d138f3dc3
ÁLVARO GÓMEZ CUENCA <algocue1@upv.edu.es>
2020-04-24 18:53:19 +0200

    Implementado menú pausa en BoardView

--- Twins/Twins/Views/Pause.xaml.cs
+++ Twins/Twins/Views/Pause.xaml.cs
@@ -9,13 +9,20 @@ using Xamarin.Forms.Xaml;
 namespace Twins.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Pause : ContentPage
+    public partial class Pause : StackLayout
     {
         public Pause(bool pausaTiempo)
         {
             InitializeComponent();
             notPause.IsVisible = pausaTiempo;
         }
-        
+
+        public Pause()
+        {
+            InitializeComponent();
+            notPause.IsVisible = false;
+        }
+
+        public void RenaunudarClicked(object sender, EventArgs e) { MenuPausa.IsVisible = false; }
     }
 }
