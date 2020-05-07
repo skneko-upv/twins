dbdeceb5a8998463042f8042a977f77cb1dc74d3
ÁLVARO GÓMEZ CUENCA <algocue1@upv.edu.es>
2020-05-07 18:07:12 +0200

    Arreglado bug cronómetro cuando ganas partida y creada clase AudioPlayer

---
+++ Twins/Twins/Models/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Twins.Models
+{
+    class AudioPlayer
+    {
+        private Stream player;
+        public AudioPlayer()
+        {
+            //player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+        }
+    }
+}
--- Twins/Twins/Models/Game.cs
+++ Twins/Twins/Models/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Twins.Model;
 using Twins.Models.Properties;
 
