cf532f54aee353f45e154e0e74ff1c42684ebbcf
jesgarsas <jesgarsas@gmail.com>
2020-05-09 01:09:53 +0200

    Clase de base datos implementada

---
+++ Twins/Twins/Persistence/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace Twins.Persistence
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _database;
+        private static readonly Lazy<Database> instance = new Lazy<Database>(() => new Database(Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.LocalApplicationData), "database.db3"))); 
+
+        private Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            //_database.CreateTableAsync<T>().Wait();
+        }
+
+        public static Database Instance
+        {
+            get 
+            {
+                return instance.Value;
+            }
+        }
+    }
+}
