283867cab950cd9eee2ee1701d1d69140ad8c26b
ÁLVARO GÓMEZ CUENCA <algocue1@upv.edu.es>
2020-05-11 03:49:14 +0200

    Implementada clase AudioPlayer

---
+++ Twins/Twins/Utils/AudioPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twins.Utils
+{
+    public class AudioPlayer
+    {
+        public Plugin.SimpleAudioPlayer.ISimpleAudioPlayer Player { get; }
+
+        public AudioPlayer() { Player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current; }
+
+        public void LoadSong(String songName)
+        {
+            Player.Load("Sounds\\" + songName);
+            Player.Play();
+        }
+    }
+}
+
+    
