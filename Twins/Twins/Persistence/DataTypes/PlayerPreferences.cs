a94a213d4b4612f068ba6463a881efa35d5d7a2a
Neko <software@skneko.moe>
2020-05-13 20:17:38 +0200

    He cambiado esto porque parece que sea la configuración por defecto y no la guardada

--- Twins/Twins/Persistence/DataTypes/PlayerPreferences.cs
+++ Twins/Twins/Persistence/DataTypes/PlayerPreferences.cs
@@ -1,24 +1,20 @@
 ﻿using SQLite;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Twins.Persistence.DataTypes
 {
    [Table ("DefaultConfiguration")]
-    class DefaultConfiguration
+    class PlayerPreferences
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
 
-        public int Column { get; set; }
+        public int Columns { get; set; }
 
-        public int Row { get; set; }
+        public int Rows { get; set; }
 
         public List<string> Decks { get; set; }
 
         public string SelectedDeck { get; set; }
-
-         
     }
 }
