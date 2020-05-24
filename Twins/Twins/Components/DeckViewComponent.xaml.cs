992ab9c0709b4ac19babbde99922f2171266323f
ÁLVARO GÓMEZ CUENCA <algocue1@upv.edu.es>
2020-05-24 14:32:46 +0200

    Actualizacion periodica

---
+++ Twins/Twins/Components/DeckViewComponent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Twins.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeckViewComponent : StackLayout
+    {
+        public DeckViewComponent()
+        {
+            InitializeComponent();
+            //BuildDeckView();
+        }
+
+        private void BuildDeckView()
+        {
+            //BackCardImage.Source = "Assets/Decks/Deck1/backimage.png";
+            //FrontCardImage.Source = "Assets/Decks/Deck1/card1.png";
+        }
+    }
+}
