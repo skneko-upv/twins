9d3cb8c821ba98d3498b498be6d7432d3b323bd4
jesgarsas <jesgarsas@gmail.com>
2020-05-26 11:57:07 +0200

    Arreglado boton next en modos libres

--- Twins/Twins/Views/ResumeGameView.xaml.cs
+++ Twins/Twins/Views/ResumeGameView.xaml.cs
@@ -20,6 +20,11 @@ namespace Twins.Views
             set => PointsLabel.Text = value.ToString();
         }
 
+        internal void DisbleNextButton()
+        {
+            NextButton.IsVisible = false;
+        }
+
         public TimeSpan Time {
             set => TimeLabel.Text = string.Format("{0:D2}:{1:D2}", value.Minutes, value.Seconds);
         }
