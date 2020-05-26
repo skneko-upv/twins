3f1f5f35505cff604005ed8d2f187fc39ed69e78
Neko <software@skneko.moe>
2020-05-26 13:24:28 +0200

    Añadir iconos y cambiar nombres y descripciones

--- Twins/Twins.UWP/Properties/AssemblyInfo.cs
+++ Twins/Twins.UWP/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Twins.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Twins")]
+[assembly: AssemblyDescription("¡Encuentra todas las parejas!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Twins.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("SAL-MOREJOS / Grupo 8")]
+[assembly: AssemblyProduct("Twins")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,3 +27,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("es")]
